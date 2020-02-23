571e6399dfafe5328aa6c190e3fb4b9e3d54ec30
GGHYTF <43428247+SRS531@users.noreply.github.com>
2020-02-23 19:44:05 +0700

    Update mpi_var2.cpp

--- lab_1/mpi_var2.cpp
+++ lab_1/mpi_var2.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <mpi.h>
 
-#define N 500
+#define N 10
 #define eps 0.00001
 
 //Count of rows to one process
@@ -48,7 +48,7 @@ double* create_vector_b(int rank, int* block_sizes){
 
 
 //Multiple matrix by vector
-double* Matrix_by_vector(double *M, double *V, int rank, int size, int* block_sizes, int* offset_posit)
+double* Matrix_by_vector(double *M, double *V, int rank, int size, int* block_si                                                                                                                zes, int* offset_posit)
 {
     int row_count_proc = block_sizes[rank];
     int offset = offset_posit[rank];
@@ -65,11 +65,11 @@ double* Matrix_by_vector(double *M, double *V, int rank, int size, int* block_si
 
         for(int i = 0; i < block_sizes[rank]; i++){
                 for(int j = 0; j < block_sizes[process_data_ind]; j++){
-                        result[i] += M[i*N + j + offset_posit[process_data_ind]] * v[j];
+                        result[i] += M[i*N + j + offset_posit[process_data_ind]]                                                                                                                 * v[j];
                 }
         }
 
-        MPI_Sendrecv_replace(v, N/size + 1, MPI_DOUBLE, (rank + 1)% size, 100, (rank - 1)% size, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv_replace(v, N/size + 1, MPI_DOUBLE, (rank + 1)% size, 100, (                                                                                                                rank - 1)% size, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     }
     return result;
@@ -110,9 +110,21 @@ void print_matrix(double* matrix, int rank, int size){
                 if(rank == process){
                         for(int i = 0; i < part_size; i++){
                                 for(int j = 0; j < N; j++){
-                                        printf("%0.0f", matrix[i*N + j]);
+                                        printf("%0.0f  ", matrix[i*N + j]);
                                 }
                                 printf("\n");
+                        }
+                }
+        }
+}
+
+//print vector
+void print_vector(double* vector, int rank, int size, int* part_sizes){
+        for(int proc = 0; proc < size; proc++){
+                MPI_Barrier(MPI_COMM_WORLD);
+                if(rank == proc){
+                        for(int i = 0; i < part_sizes[rank]; i++){
+                                printf("%0.4f\n", vector[i]);
                         }
                 }
         }
@@ -126,7 +138,7 @@ double get_chisl_tau(double* Ay, double* Y, int rank, int* block_sizes){
                 part_chisl += Ay[i]*Y[i];
         }
 
-        MPI_Allreduce(&part_chisl, &chisl, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&part_chisl, &chisl, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORL                                                                                                                D);
         return chisl;
 }
 
@@ -150,7 +162,7 @@ double get_crit_1(double *Ax, double *b, int rank, int* block_sizes){
                 part_crit += pow(Ax[i] - b[i], 2);
         }
 
-        MPI_Allreduce(&part_crit, &crit, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&part_crit, &crit, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD)                                                                                                                ;
         return crit;
 }
 
@@ -162,12 +174,12 @@ double get_crit_2(double *b, int rank, int* block_sizes){
                 part_crit += pow(b[i], 2);
         }
 
-        MPI_Allreduce(&part_crit, &crit, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&part_crit, &crit, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD)                                                                                                                ;
         return crit;
 }
 
 //multiple vector by scalar
-double* num_by_vector(double scalar, double* vector, int rank, int* block_sizes){
+double* num_by_vector(double scalar, double* vector, int rank, int* block_sizes)                                                                                                                {
         double* result = (double*)calloc(block_sizes[rank], sizeof(double));
         for(int i = 0; i < block_sizes[rank]; i++){
                 result[i] = scalar * vector[i];
@@ -176,7 +188,7 @@ double* num_by_vector(double scalar, double* vector, int rank, int* block_sizes)
 }
 
 //Minimal Nevazki maethod
-double* Minimal_Nevazki(double *A, double *b, int rank, int size, int* block_sizes, int* offset_posit)
+double* Minimal_Nevazki(double *A, double *b, int rank, int size, int* block_siz                                                                                                                es, int* offset_posit)
 {
     double* X = (double*)calloc(N, sizeof(double));
 
@@ -185,11 +197,11 @@ double* Minimal_Nevazki(double *A, double *b, int rank, int size, int* block_siz
     double del_Tau;
 
     while(1){
-        double* Ax = Matrix_by_vector(A, X, rank, size, block_sizes, offset_posit);
+        double* Ax = Matrix_by_vector(A, X, rank, size, block_sizes, offset_posi                                                                                                                t);
 
         double* Y =  differ_vectors(Ax, b, rank, block_sizes);
 
-        double* Ay = Matrix_by_vector(A, Y, rank, size, block_sizes, offset_posit);
+        double* Ay = Matrix_by_vector(A, Y, rank, size, block_sizes, offset_posi                                                                                                                t);
 
         chisl_Tau = get_chisl_tau(Ay, Y, rank, block_sizes);
         del_Tau = get_del_tau(Ay, rank, block_sizes);
@@ -209,6 +221,7 @@ double* Minimal_Nevazki(double *A, double *b, int rank, int size, int* block_siz
         }
 
         chisl_Tau = chisl_Tau/del_Tau;
+        //printf("Rank:%d  Tau:%lf\n", rank, chisl_Tau);
         double* ty = num_by_vector(chisl_Tau, Y, rank, block_sizes);
         X = differ_vectors(X, ty, rank, block_sizes);//Update Xn
         free(ty);
@@ -227,16 +240,14 @@ int main(int argc, char **argv) {
 
     double *A = create_matrix_A(rank, block_sizes, offset_positions);
     double *b = create_vector_b(rank, block_sizes);
+    //print_vector(b, rank, size, block_sizes);
 
     //print_matrix(A, rank, size);
     double start = MPI_Wtime();
-    double *X =  Minimal_Nevazki(A, b, rank, size, block_sizes, offset_positions);
+    double *X =  Minimal_Nevazki(A, b, rank, size, block_sizes, offset_positions                                                                                                                );
     double end = MPI_Wtime();
 
-    /*if(rank == 0){
-     for(int i = 0; i < N; i++)
-        printf("X[%d]= %lf\n", i, X[i]);
-    }*/
+    print_vector(X, rank, size, block_sizes);
 
     printf("Time taken: %lf sec.\n", end - start);
 
